b0169ae87abea6c9a326e98f8ac7a44804974799
singh <singh@Sadanand>
2024-06-11 19:17:38 +0530

    ID0311/committingnewtest

---
+++ src/test/java/seleniumclass/TestClass2git.java
@@ -0,0 +1,18 @@
+package seleniumclass;
+
+import org.testng.annotations.*;
+
+public class TestClass2git {
+	@Test
+public void test() {
+	System.out.println("Hello");
+}
+	@BeforeTest
+	public void Testbefore() {
+		System.out.println("Hello Before");
+	}
+	@AfterTest
+	public void Testafter() {
+		System.out.println("Hello after");
+	}
+}
